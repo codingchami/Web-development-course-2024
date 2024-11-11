86cff8f18ba4d29fffd70efd75cdee53a17854ee
codingchami <cschamithsandeepa@gmail.com>
2024-11-12 00:02:18 +0530

    animation create using key frames

---
+++ practical_session02/script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector("box");
+
+addEventListener("pointermove", (event)=>{
+    console.log("X :" + event.clientX)
+    console.log("Y :" + event.clientY)
+
+    box.style.top = 
+})
